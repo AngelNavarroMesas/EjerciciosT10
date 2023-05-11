967a0dea755e30bb5c6b398faf9510e20676d5ca
anavarro <anavarro@106-30.info2>
2023-05-11 13:38:00 +0200

    falta crear el main y ejercicios 2 y 3

--- EjerciciosT10/src/ejercicio1/Utils.java
+++ EjerciciosT10/src/ejercicio1/Utils.java
@@ -1,5 +1,6 @@
 package ejercicio1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utils {
@@ -7,17 +8,25 @@ public class Utils {
 	public static int readInt() {
 		Scanner sc = new Scanner(System.in);
 		int i=0;
-		
-		
+		try {
+			System.out.println("Introduzca un numero");
+			i = sc.nextInt();
+		}catch(InputMismatchException e) {
+			System.out.println("El valor introducido no es de tipo int");
+		}
 		return i;
 	}
 	
 	public static double readDouble() {
 		Scanner sc = new Scanner(System.in);
-		double i=0;
-		
-		
-		return i;
+		double x=0;
+		try {
+			System.out.println("Introduzca un numero");
+			x = sc.nextDouble();
+		}catch(InputMismatchException e) {
+			System.out.println("El valor introducido no es de tipo double");
+		}
+		return x;
 	}
 	
 }
