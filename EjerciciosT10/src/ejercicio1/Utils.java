569761f9ab25e15cb140669b65aec463c8979649
anavarro <anavarro@106-30.info2>
2023-05-11 13:29:38 +0200

    comit

---
+++ EjerciciosT10/src/ejercicio1/Utils.java
@@ -0,0 +1,23 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Utils {
+	
+	public static int readInt() {
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+		
+		
+		return i;
+	}
+	
+	public static double readDouble() {
+		Scanner sc = new Scanner(System.in);
+		double i=0;
+		
+		
+		return i;
+	}
+	
+}
